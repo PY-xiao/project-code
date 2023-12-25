0ae223d17a7e3846374798f57922f92f13a7145a
PY-xiao <275274742@qq.com>
2023-12-25 19:25:12 +0800

    14214

--- 源码/实例-01 图片识别/图片识别.py
+++ 源码/实例-01 图片识别/图片识别.py
@@ -1,10 +1,12 @@
 ##coding=utf-8
-#根据肤色数量判断
+# 根据肤色数量判断
 from PIL import Image
-basedir=r'H:\f-jc\00-fxm-tb-zy\【python】\py-0002 python源码\00-上传\03 实战小项目\Python图片识别\imag2'
+
+basedir = r'./imag2'
 import os
+
 for filename in os.listdir(basedir):
-    full_filename=os.path.join(basedir,filename)
+    full_filename = os.path.join(basedir, filename)
     img = Image.open(full_filename).convert('YCbCr')
     w, h = img.size
     data = img.getdata()
@@ -13,4 +15,4 @@ for filename in os.listdir(basedir):
         y, cb, cr = ycbcr
         if 86 <= cb <= 117 and 140 <= cr <= 168:
             cnt += 1
-    print('%s is a porn image?:%s.'%(filename, 'Yes' if cnt > w * h * 0.3 else 'No'))
+    print('%s is a porn image?:%s.' % (filename, 'Yes' if cnt > w * h * 0.3 else 'No'))
